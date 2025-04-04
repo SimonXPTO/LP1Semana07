cf1a48ced3d480584a4ca27265de2bc3647144d8
SimonXPTO <simaodurao@gmail.com>
2025-04-04 17:16:22 +0100

    Added MaxHealth And Health Properties

--- MyRoguelike/Hero.cs
+++ MyRoguelike/Hero.cs
@@ -4,6 +4,42 @@ namespace MyRoguelike
     {
         private int xp;
         private float health;
+        private readonly string name;
+        public string Name => name;
         
+        public int XP
+        {
+            get => xp;
+            set
+            {
+                if (value > xp)
+                {
+                    xp = value;
+                }
+            }
+        }
+
+        public int Level => (int)(XP / 1000) + 1;
+
+        private float MaxHealth => 100 + (Level - 1) * 20;
+        public float Health
+        {
+            get => health;
+            set
+            {
+                if (value < 0)
+                {
+                    health = 0;
+                }
+                else if (value > MaxHealth)
+                {
+                    health = MaxHealth;
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
     }
 }
