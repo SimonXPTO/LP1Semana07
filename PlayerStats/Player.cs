4bdc182b60122e159f9443e642fd6a66d7a235d1
SimonXPTO <simaodurao@gmail.com>
2025-04-04 16:51:40 +0100

    PlayedGames Error

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -40,14 +40,7 @@ namespace PlayerStats
 
         }
 
-        public void PlayGame(bool win)
-        {
-            playedGames++;
-            if (win)
-            {
-                wonGames++;
-            }
-        }
+        
 
         public Player(string name)
         {
