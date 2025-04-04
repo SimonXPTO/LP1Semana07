3d42f89a8f7f9fcd53e9149d53c29f462b96474a
SimonXPTO <simaodurao@gmail.com>
2025-04-04 16:51:14 +0100

    Added Highscore

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -10,7 +10,17 @@ namespace PlayerStats
         public string Name { get; }
 
 
-        
+        public float HighScore
+        {
+            get => highScore;
+            set
+            {
+                if (value > highScore)
+                {
+                    highScore = value;
+                }
+            }
+        }
 
         public float WinRate
         {
